49d4c93a1589d61bf2e172bb08168091ab5a141f
Fraser Reilly <26841319+fraserreilly@users.noreply.github.com>
2021-09-21 17:03:35 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,189 @@
+require("dotenv").config();
+
+const config = require("./config.json");
+const tmi = require("tmi.js");
+const axios = require("axios");
+
+const currentlyConnected = new Set();
+const userIDs = new Set()
+
+const client = new tmi.Client({
+	options: { debug: false },
+	identity: {
+		username: config.nick,
+		password: process.env.TWITCHOAUTH
+	},
+	channels: []
+});
+
+async function getPersonalUserID(){
+    response = await axios.get(`${config.twitchAPI}users`, {
+        params: {
+            "login": config.nick
+        },
+        headers: {
+        "Authorization": `Bearer ${process.env.BEARER}`,
+        "Client-Id": `${process.env.CLIENT_ID}`
+    }})
+    .catch(function(e) {
+        console.log(e.response);
+    })
+    return response.data.data[0].id
+
+}
+
+async function getChannels() {
+    if (config.watchFollowed === true) {
+        const userID = await getPersonalUserID();
+        const response = await axios.get(`${config.twitchAPI}streams/followed`, {
+            params: {
+                user_id: userID,
+                cursor: "",
+                first: 100
+            },
+        headers: {
+            "Authorization": `Bearer ${process.env.BEARER}`,
+            "Client-Id": `${process.env.CLIENT_ID}`
+        }})
+        .catch(function (e) {
+            console.log(e.response);
+        })
+        for (let user in response.data.data) {
+            userIDs.add(response.data.data[user].user_login)
+        }
+    }
+    if (config.channels.length > 0) {
+        const response = await axios.get(`${config.twitchAPI}streams`, {
+            params: {
+                cursor: "",
+                first: 100,
+                user_login: config.channels
+            },
+        headers: {
+            "Authorization": `Bearer ${process.env.BEARER}`,
+            "Client-Id": `${process.env.CLIENT_ID}`
+        }})
+        .catch(function (e) {
+            console.log(e.response);
+        })
+        for (let user in response.data.data) {
+            userIDs.add(response.data.data[user].user_login)
+        }
+    }
+    const params = {
+        after: "",
+        first: 100
+    }
+    if (config.games.length > 0) {
+        const gamesList = []
+        const response = await axios.get(`${config.twitchAPI}games`, {
+            params: {
+                "name": config.games
+            },
+            headers: {
+                "Authorization": `Bearer ${process.env.BEARER}`,
+                "Client-Id": `${process.env.CLIENT_ID}`
+            }})
+            .catch(function(e) {
+                console.log(e.response);
+            })
+            for (let gameID in response.data.data) {
+                if (response.data.data[gameID].id === undefined) {
+                    break
+                }
+                gamesList.push(response.data.data[gameID].id)
+            }
+        params["game_id"] = gamesList
+    }
+
+    const response = await axios.get(`${config.twitchAPI}streams`, {
+        params: params,
+        headers: {
+            "Authorization": `Bearer ${process.env.BEARER}`,
+            "Client-Id": `${process.env.CLIENT_ID}`
+        }})
+    .catch(function (e) {
+        console.log(e.response);
+    })
+    for (let user in response.data.data) {
+        if (response.data.data[user].viewer_count <= config.minViewers || response.data.data[user].viewer_count >= config.maxViewers || response.data.data[user].user_login === undefined) {
+            break;
+        }
+        userIDs.add(response.data.data[user].user_login)
+    }
+    if (userIDs.size > config.maxChannels) {
+        userIDs.size = config.maxChannels
+    }
+    return userIDs
+}
+
+async function joinChannels(){
+    client.on("connected", onConnectedHandler);
+    await client.connect()
+    const channels = Array.from(await getChannels());
+    if (channels.length > config.maxChannels) {
+        channels.length = config.maxChannels
+    }
+    console.log(`Connecting to ${channels.length} channels`)
+    for (let user in channels) {
+            client.join(channels[user]);
+            console.log(`${config.nick} successfully joined ${channels[user]}'s stream`);
+            currentlyConnected.add(channels[user]);
+            await sleep(1000);
+    }
+}
+
+function onConnectedHandler (addr, port) {
+    console.log(`${config.nick} Connected to ${addr}:${port}`);
+}
+
+async function removeInactiveChannels() {
+    const response = await axios.get(`${config.twitchAPI}streams`, {
+        params: {
+            cursor: "",
+            first: 100,
+            user_login: Array.from(currentlyConnected)
+        },
+    headers: {
+        "Authorization": `Bearer ${process.env.BEARER}`,
+        "Client-Id": `${process.env.CLIENT_ID}`
+    }})
+    .catch(function (e) {
+        console.log(e.response);
+    })
+    for (let user in response.data.data) {
+        if (response.data.data[user].user_login in Array.from(currentlyConnected)) {
+            break;
+        }
+        userIDs.add(response.data.data[user].user_login)
+    }
+}
+async function updateChannels() {
+    await removeInactiveChannels();
+    const channels = Array.from(await getChannels());
+    for (let user in channels) {
+        if (currentlyConnected.size < config.maxChannels) {
+            client.join(channels[user]);
+            currentlyConnected.add(channels[user]);
+            console.log(`${config.nick} successfully joined ${channels[user]}'s stream`);
+            await sleep(1000);
+        } else {
+            console.log(`${config.nick} already connected to ${currentlyConnected.size} streams, sleeping`);
+            return;
+        }
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function run() {
+    joinChannels();
+    while (true) {
+        await sleep(1200000);
+        updateChannels();
+    }
+}
+
+run()
---
+++ readme.md
@@ -0,0 +1,51 @@
+# twitch-subscription-farmer
+
+## About
+Node.js application that allows you to farm channels for gifted subs
+
+## Installation
+
+1. Check out twitch-subscription-farmer to your desired directory
+```
+$ git clone https://github.com/fraserreilly/twitch-farmer your_directory
+```
+2. Install node_modules
+```
+$ npm i while in your directory or run the initialize.bat file
+```
+## Usage
+### Prerequisites
+You need to get your OAUTH token, bearer token and client ID. 
+First get your bearer token [here](https://twitchapps.com/tmi/).  
+Then get your client ID by following the steps [here](https://dev.twitch.tv/docs/api/) **you'll need this to get your OAUTH token also**.
+Then get your OAUTH token [here](https://twitchapps.com/tokengen/) **make sure to include "user:read:follows" in the scopes field**.
+After getting your OAUTH token, bearer token and client ID, rename "_template.env" to ".env" and replace the made up values:
+```
+TWITCHOAUTH="OAUTH"
+BEARER='bearer'
+CLIENT_ID="client_id"
+```
+**Change "config_template.json" to "config.json"**
+**Make sure you change the nick in "config.json" to your twitch username in lower case**
+### (Optional) Configuration
+There are several options you can tweak in the "config.json" file:
+* "maxChannels": The maximum amount of channels the application will try and connect to
+* "minViewers": The maximum amount of viewers per channel the application will try and connect to
+* "maxViewers": The maximum amount of viewers per channel the application will try and connect to
+* "watchFollowed": Change to true if you want the application to connect to your followed live streamers **the application will always connect to these before other streams**
+* "channels": List of channels the application will try and connect to, leave empty if you don't have any specific channels to watch that you don't follow **the application will always connect to these before other streams but after followed streams**
+* "games": List of games that the application will get channels from, leave empty if you don't want to get streams from specific games
+
+Examples of games and channels:
+```
+channels = ["sodapoppin", "tfue", "loltyler1"] **make sure that these names are lowercase**
+games = ["VALORANT", "Just Chatting", "League of Legends"] **make sure this is the full name of the game**
+
+### Running the application
+To run the application open the start.bat file or execute:
+```
+node index.js
+```
+
+## DISCLAIMER
+twitch-farmer was made for personal use only. If you find any improvments or bugs in the code let me know. If you have any problems while running this app, please refer to above.
